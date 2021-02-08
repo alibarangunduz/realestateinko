518fd595ed8f14df766f05f7c2a76bb1e6f08b2c
Ali Baran <alibarangunduzz@gmail.com>
2021-02-08 16:22:33 +0300

    added firebase auth

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-    apiKey: "AIzaSyCVGQEKU-O4NI0gwliD47ks-E_UT62TZHQ",
-    authDomain: "realestate-fbe82.firebaseapp.com",
-    projectId: "realestate-fbe82",
-    storageBucket: "realestate-fbe82.appspot.com",
-    messagingSenderId: "496897253628",
-    appId: "1:496897253628:web:540653e34151bcefd7f779",
-    measurementId: "G-W6HP67GWL0"
+    
   };
   const fire = firebase.initializeApp(firebaseConfig);
   export default fire;
