9b2809a625191f17b30a62dc48d3ff4a17406c58
Ali Baran Gündüz <58346472+alibarangunduz@users.noreply.github.com>
2021-04-02 02:13:42 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,6 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "realestate-fbe82.firebaseapp.com",
-  projectId: "realestate-fbe82",
-  storageBucket: "realestate-fbe82.appspot.com",
-  messagingSenderId: "496897253628",
-  appId: "1:496897253628:web:540653e34151bcefd7f779",
-  measurementId: "G-W6HP67GWL0",
 };
 const fire = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
